0a071294339b0cf9211f73bc9985f8fbf5deb990
Arthur Wu (a.k.a Kuan-Ting Wu) <wu1106@purdue.edu>
2017-01-31 00:23:05 -0500

    Finished quickStart video2&3

--- Flicks/MovieViewController.swift
+++ Flicks/MovieViewController.swift
@@ -11,13 +11,34 @@ import UIKit
 class MovieViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
 {
 
+   
     
     @IBOutlet var tableView: UITableView!
+    
+    var movies : [NSDictionary]?
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         tableView.dataSource = self;
         tableView.delegate = self;
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    self.movies = dataDictionary["results"] as! [NSDictionary]
+                    self.tableView.reloadData();
+                }
+            }
+        }
+        task.resume()
+
+        
         // Do any additional setup after loading the view.
     }
 
@@ -27,15 +48,29 @@ class MovieViewController: UIViewController,UITableViewDataSource,UITableViewDel
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 20;
+        if let movies = movies
+        {
+            return movies.count;
+        }
+        else
+        {
+            return 0;
+        }
+        
+        
     }
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell;
         
-        cell.textLabel!.text = "row \(indexPath)";
+        let movie = movies![indexPath.row];
+        let title = movie["title"] as! String;
+        let overview = movie["overview"] as! String;
+        
         print("row \(indexPath)")
         
+        cell.titleLabel.text = title;
+        cell.overviewLabel.text = overview;
         
         return cell;
     }
---
+++ MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Arthur on 2017/1/31.
+//  Copyright © 2017年 Kuan-Ting Wu (Arthur Wu). All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
